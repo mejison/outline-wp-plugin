d5237f5ae585613471164ffa7054fbfbca8cba74
mejison <dev.mejison@gmail.com>
2022-02-17 15:49:43 +0200

    commit

--- class.outline.php
+++ class.outline.php
@@ -5,9 +5,9 @@ class Outline {
 	const API_PORT = 80;
 
     public static function init() {
-		if( is_admin() || current_user_can('editor') || current_user_can('administrator')) {
-			return;
-		}
+		// if( is_admin() || current_user_can('editor') || current_user_can('administrator')) {
+		// 	return;
+		// }
 
 		$onOff = get_option('on_off');
 		if ($onOff == 'on') {
